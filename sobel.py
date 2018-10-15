c60efcdedd0e8d45c12368345a650a189cbcadb0
Hugggsy <hugo.depretz@gmail.com>
2018-10-15 16:06:00 +0100

    fonctions contraste et rogner

--- sobel.py
+++ sobel.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from copy import copy
 
 from skimage.data import camera
 from skimage.filters import roberts, sobel, scharr, prewitt, laplace
@@ -7,17 +8,36 @@ from skimage import io
 
 img = io.imread('Images/face_fil.jpg', as_gray=True)
 
-edge_roberts = roberts(img)
 edge_sobel = sobel(img)
-edge_scharr = scharr(img)
-edge_prewitt = prewitt(img)
-edge_laplace = laplace(img)
+
+
+def augmentContrast(mat, seuil):
+    for y in range(mat.shape[0]):
+        for x in range(mat.shape[1]):
+            if mat[y][x] > seuil:
+                mat[y][x] = 1
+            else:
+                mat[y][x] = 0
+    return mat
+
+
+def rogner(mat, xMin, xMax, yMin, yMax):
+    matRogne = mat[yMin:yMax, xMin:xMax]
+    print(matRogne.shape)
+    return matRogne
+
+
+contraste = augmentContrast(edge_sobel, 0.1)
+print(contraste.shape)
+
+rogne = rogner(contraste, 1000, 3600,  200, 2400)
+
 
 fig, ax = plt.subplots(ncols=2, sharex=True, sharey=True,
                        figsize=(20, 20))
 
 
-ax[0].imshow(edge_sobel, cmap=plt.cm.gray)
+ax[0].imshow(rogne, cmap=plt.cm.gray)
 ax[0].set_title('Sobel Edge Detection')
 
 
