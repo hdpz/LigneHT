dcea19e15963ab2029fda764cf5e405f29226e77
Hugggsy <hugo.depretz@gmail.com>
2018-10-15 12:21:04 +0100

    exemple

---
+++ script.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import ndimage as ndi
+
+from skimage import feature
+
+
+# Generate noisy image of a square
+im = np.zeros((128, 128))
+im[32:-32, 32:-32] = 1
+
+im = ndi.rotate(im, 15, mode='constant')
+im = ndi.gaussian_filter(im, 4)
+im += 0.2 * np.random.random(im.shape)
+
+# Compute the Canny filter for two values of sigma
+edges1 = feature.canny(im)
+edges2 = feature.canny(im, sigma=3)
+
+# display results
+fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(8, 3),
+                                    sharex=True, sharey=True)
+
+
+fig.tight_layout()
+
+plt.show()
